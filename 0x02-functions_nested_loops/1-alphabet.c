f3830ac910d9515844d9bf929d03e4043e2b8264
keira-claudette <claudette.keira@gmail.com>
2021-02-08 20:11:32 +0000

    first commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+*function prints the alphabet in lowercase followed by a new line
+* return type is void
+*/
+
+void print_alphabet(void);
+{
+char alpha = a;
+while (a <= z)
+
+{
+_putchar('alpha');
+alpha++
+}
+_putchar('\n');
+}
