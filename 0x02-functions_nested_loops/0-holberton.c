a3164ebd4b80d99263d45ac07dc439556789eef8
keira-claudette <claudette.keira@gmail.com>
2021-02-08 14:54:47 +0000

    first commit

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*main prints Holberton followed by  new line.
+*return (0)success
+*/
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+
+ return  (0);
+}
