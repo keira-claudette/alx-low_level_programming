37517ac87295b1f55ef6deb4660bfe9fb65d2b7b
keira-claudette <claudette.keira@gmail.com>
2021-02-10 10:06:07 +0000

    correction

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
 * _islower - checks for lowercase character
-* @c - the arguement that will be checked
+* @c: the arguement that will be checked
 * Return: (1) if c is lowercase, 0 otherwise
 */
 int _islower(int c)
