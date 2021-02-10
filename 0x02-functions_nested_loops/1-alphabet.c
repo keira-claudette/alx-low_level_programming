692625aeef5e60d1ca61c8cc09717289a8163a68
keira-claudette <claudette.keira@gmail.com>
2021-02-10 08:05:01 +0000

    changes to variable

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
-*function prints the alphabet in lowercase followed by a new line
-* return type is void
+*print_alphabet - prints the alphabet in lowercase followed by anew line
+* return void
 */
 
 void print_alphabet(void);
