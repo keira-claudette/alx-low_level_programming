50bd15ad3bee3910d52ffc2f1e278d07fda5c821
keira-claudette <claudette.keira@gmail.com>
2021-02-15 17:58:54 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * int_strlen - returns length of string
+ * @s: string to be checked
+ * @i: index of elements of the array
+ * Return: void
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+
+	++i;
+
+	return (i);
+}
