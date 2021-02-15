b8b4960c999bc578f94391778f1f4ce031142139
keira-claudette <claudette.keira@gmail.com>
2021-02-15 20:54:49 +0000

    correcting value of j

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,6 +11,7 @@ void print_rev(char *s)
 	int i;
 	int j;
 	int last_char;
+
 	i = 0;
 
 	while (s[i] != '\0')
@@ -20,7 +21,7 @@ void print_rev(char *s)
 
 	last_char = i;
 
-	j = last_char;
+	j = last_char - 1;
 
 	while (j >= 0)
 	{
