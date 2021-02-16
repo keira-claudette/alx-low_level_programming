c32c893e8aaf2cb0e2089a0eac7ef1871644ab18
keira-claudette <claudette.keira@gmail.com>
2021-02-16 17:41:43 +0000

    reducing line to below 80 characters

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * *_strcpy - copies the string pointed to by src including the terminating null
+ * _strcpy - copies the string pointed to by src
+ * including the terminating null
  * byte (\0) to the buffer pointed to by dest
  * @dest: pointer to the buffer we copy the string
  * @src: string to be copied
