fc91a9c0ed2931d4d2b80db111ed5ea8d7390fa0
keira-claudette <claudette.keira@gmail.com>
2021-02-16 15:34:07 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character strating with the first.
+ * @str: string
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+		while (str[i] != '\0')
+		{
+
+			if (i % 2 == 0)
+			{
+				_putchar(str[i]);
+			}
+			i++;
+		}
+	_putchar('\n');
+}
