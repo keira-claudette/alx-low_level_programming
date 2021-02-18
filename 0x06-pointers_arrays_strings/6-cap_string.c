6c42bbfc677d75fafd6ec7bd5de7f17c427cde33
keira-claudette <claudette.keira@gmail.com>
2021-02-18 18:37:33 +0000

    first commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize first letter of each word
+ * @s: string to manipulate
+ * Return: string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	/* first checking  index for lowercaps */
+
+	if (s[i] >= 'a' && s[i] <= 'z')
+
+	i++;
+
+	while (s[i] != '\0')
+	{
+
+		/* if lowercase and prior char is separator, capitalize*/
+		if ((s[i] >= 'a' && s[i] <= 'z')
+		    && (s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' ||
+			s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '"' ||
+			s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' ||
+			s[i - 1] == '}' || s[i - 1] == ' ' || s[i - 1] == '\t'
+			|| s[i - 1] == '\n'))
+			s[i] = s[i] - 'a' + 'A';
+		i++;
+	}
+
+	return (s);
+}
