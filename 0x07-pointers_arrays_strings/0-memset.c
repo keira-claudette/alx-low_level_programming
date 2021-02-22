3bedcaab1a81336251f32c30f0cd1ec034e7847f
keira-claudette <claudette.keira@gmail.com>
2021-02-22 11:33:10 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _memset - function that fills memory with a constant byte
+ * @s: string to be checked
+ * @b: constant byte to be filled
+ * @n: the first bytes in memory to be filled.
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (i < n)
+			s[i] = b;
+	}
+	return (s);
+
+
+}
