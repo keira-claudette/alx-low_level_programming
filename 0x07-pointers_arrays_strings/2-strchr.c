052c5d0e2bb5423803720fffeaa44147b9f8f616
keira-claudette <claudette.keira@gmail.com>
2021-02-22 13:30:07 +0000

    first commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @s: pointer to a string to be checked
+ * @c: character to be checked
+ * Return: pointer to the fist occurrence of c or NULL if no occurrence
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+		if (s[i] == c)
+			return ((s + i));
+
+			return (0);
+}
