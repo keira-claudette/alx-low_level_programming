b83d6110b09dfab423d07fd4896a2fd7ec72638b
keira-claudette <claudette.keira@gmail.com>
2021-02-24 18:05:50 +0000

    first commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * find_prime - finds prime number
+ * @a: number to be checked
+ * @b: number to iterate and check
+ */
+int find_prime(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (find_prime(a, b + 1));
+}
+/**
+ * is_prime_number - checks for prime number
+ * @n: integer to check
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (find_prime(n, 2));
+}
