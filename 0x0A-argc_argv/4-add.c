7a670613bdeb3e7b973e89dde492260e4e8eedfe
keira-claudette <claudette.keira@gmail.com>
2021-02-28 17:53:08 +0000

    first commit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints program name
+ * @argc: counts the arguments passed
+ * @argv: array of pointers to pointers containing arguments passed
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	int i, sum;
+	i = 0;
+
+	if (argc <= 1)
+	{
+		printf("0\n");
+	}
+	else
+		if ((*argv[i] >= 'a' && *argv[i] <= 'z') || (*argv[i] >= 'A' && *argv[i] <= 'Z'))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+
+		if (**argv > 0)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+
+			if (i == argc - 1)
+			{
+			printf("%d\n", sum);
+			}
+		}
+
+	}
+	return (0);
+}
