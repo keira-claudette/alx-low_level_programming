ad83e78987dece51aa7416747148cb9e589d85f2
keira-claudette <claudette.keira@gmail.com>
2021-03-02 11:33:15 +0000

    first commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - allocates new space in memory with a copy of the springs
+ * @str: array of chars
+ * Return: pointer to a char
+ */
+char *_strdup(char *str)
+{
+/* Find length of string */
+	int i, length, j;
+
+	char *ptr;
+
+	for (i = 0; str[i] != '\0'; i++)
+		length = i;
+/*allocate memory for copy of string*/
+	ptr = (char *)malloc(length * sizeof(char));
+
+	if (*str == '\0')
+	{
+		return (NULL);
+	}
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		ptr[j] = str[j];
+	}
+	return (ptr);
+}
