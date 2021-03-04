cfdd12b96ebb4195a8bbd74f989d2e4421d69581
keira-claudette <claudette.keira@gmail.com>
2021-03-04 20:04:37 +0000

    first commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: byyte size
+ * Return: pointer to void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	void *ptr;
+
+	ptr = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+			if (ptr == NULL)
+				return (NULL);
+			return (ptr);
+}
