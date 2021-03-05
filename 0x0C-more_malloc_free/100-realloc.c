4711bbfdcee203ab8f25eb1075705a5927c4138e
keira-claudette <claudette.keira@gmail.com>
2021-03-05 19:05:13 +0000

    first commit

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _realloc - reallocates memory using malloc
+ * @ptr: pointer to old array
+ * @old_size: size in bytes of old array
+ * @new_size: size of newly allocated memory
+ * Return: pointer to void for newly allocated memory of new_size filled with
+ * minumum of old_size or new_size bytes. or NULL if allocation fails
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (!ptr)
+		return (malloc(new_size));
+
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
+}
