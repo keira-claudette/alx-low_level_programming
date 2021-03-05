2c5f17064f3a3db7dece02009e4bfc3960acc979
keira-claudette <claudette.keira@gmail.com>
2021-03-05 17:37:36 +0000

    initiated elements to 0

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,6 +10,7 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	unsigned int i;
 
 	void *ptr;
 
@@ -22,8 +23,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 				return (NULL);
 
-/*			for (i = 0; i < nmemb; i++)
-			ptr[i] = 0; */
+			for (i = 0; i < nmemb; i++)
+			ptr[i] = 0;
 
 			return (ptr);
 }
