63e40ed62cad2f0df288dcf1fb48228bf4a5784a
keira-claudette <claudette.keira@gmail.com>
2021-03-05 17:31:43 +0000

    rearranged code

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,12 +13,17 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	void *ptr;
 
-	ptr = malloc(nmemb * size);
-
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
+	ptr = malloc(nmemb * size);
+
 			if (ptr == NULL)
+
 				return (NULL);
+
+/*			for (i = 0; i < nmemb; i++)
+			ptr[i] = 0; */
+
 			return (ptr);
 }
