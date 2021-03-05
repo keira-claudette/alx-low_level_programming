1e370eb7c1e843c6c950416b378fac9c0202d228
keira-claudette <claudette.keira@gmail.com>
2021-03-05 17:08:39 +0000

    assigned values to s3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,39 +12,36 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 /* find length of s1 and s2 */
-	unsigned int i, j, k, len1, len2, len3;
+	unsigned int i, len, len1;
 
 	char *s3;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		len1 = i;
-	for (j = 0; s2[j] != '\0'; j++)
-		len2 = j;
-	{
-		if (n >= len2)
-			len3 = len1 + len2;
-		else if (n < len2)
-		{
-		for (j = 0; j < n; j++)
-			len2 = j;
-		len3 = len1 + len2;
-		}
-	}
+	len = 0;
+
+	if (s1 != NULL)
+		for (i = 0; s1[i] != '\0'; i++)
+			len++;
+	len1 = len;
+	if (s2 != NULL)
+		if (sizeof(s2) < n)
+			for (i = 0; s2[i] != '\0'; i++)
+				len++;
+		if (sizeof(s2) > n)
+			for (i = 0; i < n; i++)
+				len++;
+
 /* allocate memory for s3 */
-	s3 = malloc((sizeof(char) * len3) + 1);
+	s3 = malloc((sizeof(char) * len) + 1);
 
 	if (s3 == NULL)
 		return (NULL);
 /* assign values to s3*/
-	for (k = 0; k < len3; k++)
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-			s3[k] = s1[1];
-		for (j = 0; j <= n; j++)
-		{
-			s3[k] = s2[j];
-		}
-	}
-	s3[k] = '\0';
+	for (i = 0; s1[i] != '\0'; i++)
+		s3[i] = s1[i];
+	for (; i < len; i++)
+		s3[i] = s2[i - len1];
+			s3[i] = '\0';
+
 	return (s3);
+
 }
