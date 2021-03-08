c48dcf0cd733b996384b78eb19ab087cd0f9e10a
keira-claudette <claudette.keira@gmail.com>
2021-03-08 10:49:33 +0000

    first commit

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of the file the function is compiled in
+ * Return: 0 to show success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
