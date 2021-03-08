af68a52c7d74b2e64b1047e1623501e8af9bc412
keira-claudette <claudette.keira@gmail.com>
2021-03-08 17:44:50 +0000

    added description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - for a dogs biodata
+ * @name: first member
+ * @age: second member
+ * @owner: Third member
+ */
 struct dog {
 	char *name;
 	float age;
