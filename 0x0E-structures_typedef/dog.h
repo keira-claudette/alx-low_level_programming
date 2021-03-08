6c962fb49fcfebd9b97851eecc81fc7288b4e94b
keira-claudette <claudette.keira@gmail.com>
2021-03-08 17:31:25 +0000

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
+struct dog{
+	char *name;
+	float *age;
+	char *owner;
+};
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
