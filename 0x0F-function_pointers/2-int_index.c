d56e69b229a4c3d69913d692f44275175c8cf462
keira-claudette <claudette.keira@gmail.com>
2021-03-10 20:21:20 +0000

    altered condition for failed check

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,14 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
-	if (array || cmp)
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (i);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
 	}
-/*	else*/
+
 	return (-1);
 }
