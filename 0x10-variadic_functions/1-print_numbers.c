1c1b9f5c068fb9e8d76860a01f6eee6a8f8a358b
keira-claudette <claudette.keira@gmail.com>
2021-03-11 19:38:32 +0000

    corrected space betty style

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		va_start(num_print, n);
 		for (i = 0; i < n - 1; i++)
 		{
-			if(!separator)
+			if (!separator)
 			{
 				printf("%d", va_arg(num_print, unsigned int));
 			}
