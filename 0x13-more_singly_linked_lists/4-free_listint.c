46d8a227b2b85f221e3a0b349bc8f50c67546617
keira-claudette <claudette.keira@gmail.com>
2021-03-22 17:18:11 +0000

    first commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *free_listint - frees a listint_t
+ * @head: pointer to struct to free
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+	while (head)
+	{
+		temp = head->next;
+
+		free(head);
+		head = temp;
+	}
+}
