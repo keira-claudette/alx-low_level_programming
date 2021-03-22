be941c826c98f7ed47f5473be820ab446a89e8ae
keira-claudette <claudette.keira@gmail.com>
2021-03-22 18:02:50 +0000

    first commit

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * pop_listint - deletes head note and returns the node's data n
+ * @head: pointer to linked list
+ * Return: data in head node
+ */
+
+int pop_listint(listint_t **head)
+{
+	int t = (*head)->n;
+	listint_t *temp;
+
+	if (!head || !*head)
+		return (0);
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+	return (t);
+}
