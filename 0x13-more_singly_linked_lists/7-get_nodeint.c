a7feb4a934a21b815f038a130bd98b2bd445c896
keira-claudette <claudette.keira@gmail.com>
2021-03-23 12:48:15 +0000

    betty error resolved

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,9 +13,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *temp = head;
 	unsigned int i = 0;
 
-/*	if (!head)
-	return (NULL);*/
-
 	while (temp && i < index)
 	{
 		temp = temp->next;
@@ -23,7 +20,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	}
 
 	if (!temp)
-		return(NULL);
+		return (NULL);
 
 	return (temp);
 }
