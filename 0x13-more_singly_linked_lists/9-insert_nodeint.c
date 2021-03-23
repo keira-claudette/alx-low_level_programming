ac2d91d43f0db42c69636dd6db6cff327739fe58
keira-claudette <claudette.keira@gmail.com>
2021-03-23 11:54:39 +0000

    corrected core dumped

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,20 +18,32 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 
 	new = malloc(sizeof(listint_t));
-	if (!new)
+	if (!new || !head)
 		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
 
 	while (temp && i < idx)
 	{
-		temp = temp->next;
 
-		if (!temp)
-			return (NULL);
-
-		(temp - 1)->next = new;
-		new->next = (temp + 1);
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+		i++;
 	}
-	return (new);
+	return (NULL);
 }
