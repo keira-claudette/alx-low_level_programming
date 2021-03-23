3883d0d3aa06ad8c988dfe24e10b1341fce431e8
keira-claudette <claudette.keira@gmail.com>
2021-03-23 12:39:10 +0000

    requirement compliance

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,14 +13,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
 
-	if (h)
+	while (h)
 	{
-		while (h)
-		{
-			count++;
-			printf("%d\n", h->n);
-			h = h->next;
-		}
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
+
 	return (count);
 }
