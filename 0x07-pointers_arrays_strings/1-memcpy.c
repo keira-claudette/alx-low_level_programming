6335669bef79050a50f4601056c8193593de1233
Claudette Mokeira <68082012+keira-claudette@users.noreply.github.com>
2021-04-22 12:44:26 +0300

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,17 +10,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
 
-for (i = 0; *(dest + i) != '\0'; i++)
+for(i = 0; i < n; i++)
 {
-for (i = 0; src[i] != '\0'; i++)
-
-{
-if (i < n)
-{
-
-*(dest + i) = *(src + i);
-}
-}
+dest[i] = src[i];
 }
 return (dest);
 
