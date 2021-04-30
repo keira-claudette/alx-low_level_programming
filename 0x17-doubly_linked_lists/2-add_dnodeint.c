c1f982d94704a797bb961ff95c065d3bfe2da4f8
keira-claudette <claudette.keira@gmail.com>
2021-04-30 18:24:40 +0000

    indented code blocks

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,19 +7,19 @@ B#include "lists.h"
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-dlistint_t *new_node = malloc(sizeof(dlistint_t));
-if (!new_node)
-return (NULL);
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+	if (!new_node)
+		return (NULL);
 
-new_node->n = n;
-new_node->prev = NULL;
-new_node->next = *head;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = *head;
 
-if (*head)
-{
-(*head)->prev = new_node;
-}
-*head = new_node;
+	if (*head)
+	{
+		(*head)->prev = new_node;
+	}
+	*head = new_node;
 
-return (new_node);
+	return (new_node);
 }
