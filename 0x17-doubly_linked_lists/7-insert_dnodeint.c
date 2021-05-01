e5187f59025db85d5a96b73316a759330b2fd247
keira-claudette <claudette.keira@gmail.com>
2021-05-01 12:38:15 +0000

    added code block for when index is end of list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -38,6 +38,12 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 		i++;
 	}
+	if (temp->prev && temp->next == NULL)
+	{
+		temp->next = new_mid_node;
+		new_mid_node->next = NULL;
+		return (new_mid_node);
+	}
 
 	new_mid_node->next = temp;
 	new_mid_node->prev = temp->prev;
