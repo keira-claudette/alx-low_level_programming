9476ac85de91036c1ea828849e820d47bfc7c947
keira-claudette <claudette.keira@gmail.com>
2021-05-02 11:53:26 +0000

    solved git conflict

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,28 +8,17 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-<<<<<<< HEAD
-=======
-	dlistint_t *new_mid_node, *prev_node;
-	dlistint_t *temp = *h;
->>>>>>> a5d952e2535b91d06e09aec35446118fd33a405e
 	unsigned int i;
 	dlistint_t *new;
 	dlistint_t *temp = *h;
 
-<<<<<<< HEAD
 	new = malloc(sizeof(dlistint_t));
 	if (!new || !h)
-=======
-	new_mid_node = malloc(sizeof(dlistint_t));
-	if (!new_mid_node || !h)
->>>>>>> a5d952e2535b91d06e09aec35446118fd33a405e
 		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
 
-<<<<<<< HEAD
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
@@ -50,33 +39,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 
 	return (NULL);
-=======
-	if (*h == NULL)
-	{
-		*h = new_mid_node;
-		new_mid_node->prev = NULL;
-		new_mid_node->next = NULL;
-		return (new_mid_node);
-	}
-	if (idx == 0 && *h)
-	{
-		new_mid_node->next = *h;
-		(*h)->prev = new_mid_node;
-		*h = new_mid_node;
-		new_mid_node->prev = NULL;
-		return (new_mid_node);
-	}
-	while (*h && i < idx && temp)
-	{
-		temp = temp->next;
-		i++;
-	}
-	new_mid_node->next = temp;
-	if (temp->next == NULL)
-		new_mid_node->next = NULL;
-	new_mid_node->prev = temp->prev;
-	prev_node = temp->prev;
-	prev_node->next = new_mid_node;
-	return (new_mid_node);
->>>>>>> a5d952e2535b91d06e09aec35446118fd33a405e
 }
