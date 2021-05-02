b3986d6ecd581aa8a4353e4317d355b1948ace07
keira-claudette <claudette.keira@gmail.com>
2021-05-02 12:00:07 +0000

    alternative code

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -8,4 +8,35 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int i;
+	dlistint_t *new;
+	dlistint_t *temp = *h;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new || !h)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	for (i = 0; temp && i < idx; i++)
+	{
+		if (i == idx - 1)
+		{
+			if (temp->next == NULL)
+				return (add_dnodeint_end(h, n));
+			new->next = temp->next;
+			new->prev = temp;
+			temp->next->prev = new;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+
+	return (NULL);
 }
