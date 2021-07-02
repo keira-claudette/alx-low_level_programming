f234f6cf3d3e82bece2c97f6a0fbde40bd153d5e
keira-claudette <claudette.keira@gmail.com>
2021-07-02 20:27:51 +0000

    reverted to attempt 1

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,25 +11,18 @@ def island_perimeter(grid):
     n_cells = 0
     list_1 = []
 
-    for i in range(len(grid)):
-        if i == 0 or i == len(grid):
+    for items in range(len(grid)):
+        if items == 0 or items == len(grid):
             continue
-        for c in range(len(grid[i])):
-            if grid[i][c] == 1:
-                if grid[i][c + 1] == 0:
-                    if grid[i][c - 1] == 1:
-                        list_1.append(c)
+        for cell in range(len(grid[items])):
+            if grid[items][cell] == 1:
+                if grid[items][cell + 1] == 0:
+                    if grid[items][cell - 1] == 1:
                         n_cells = n_cells + 1
-                    elif grid[i + 1][c] == 1 and grid[i - 1][c] and grid[i][c + 1] == 1:
-                        list_1.append(c)
-                        n_cells = n_cells + 1
-                        break
-                    elif grid[i][c] == 1 and grid[i - 1][c] and grid[i][c + 1] == 1:
-                        break
                     else:
                         continue
                 else:
-                    list_1.append(c)
+                    list_1.append(cell)
                     n_cells = n_cells + 1
             else:
                 continue
